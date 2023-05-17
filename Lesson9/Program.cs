d9f2c1353c5a37923d0bd2e964849a66cba808b0
Hakan <ahmethakanozturk34@gmail.com>
2023-05-17 20:25:25 +0300

    OnConfiguring

---
+++ Lesson9/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+Console.WriteLine();
+public class ETicaretDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-L858T4B;Database=ECommerceDb;User Id=hakan;Password=sifre1234;Encrypt=True;TrustServerCertificate=True;");
+    }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+
+}
