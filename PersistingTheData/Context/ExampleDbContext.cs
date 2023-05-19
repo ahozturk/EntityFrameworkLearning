f4e79ad2d61c8a524c39b81d8925d348c478588b
Hakan <ahmethakanozturk34@gmail.com>
2023-05-20 02:59:09 +0300

    Persisting Data
    - EF Core Add methods and SaveChanges

---
+++ PersistingTheData/Context/ExampleDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersistingTheData.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistingTheData.Context
+{
+    public class ExampleDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-L858T4B;Database=ExampleDb;User Id=hakan;Password=sifre1234;Encrypt=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ PersistingTheData/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PersistingTheData.Context;
+using PersistingTheData.Entities;
+
+Console.WriteLine("Hello, World!");
+
+ExampleDbContext _context = new();
+var product = new Product()
+{
+    Name = "Test",
+};
+
+Console.WriteLine(_context.Entry(product).State);
+await _context.AddAsync(product);
+Console.WriteLine(_context.Entry(product).State);
+await _context.SaveChangesAsync();
+Console.WriteLine(_context.Entry(product).State);
+_context.Remove(product);
+Console.WriteLine(_context.Entry(product).State);
