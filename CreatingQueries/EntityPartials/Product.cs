1b31954bce014e8a0b2d1dd495bac5b1182725d2
Hakan <ahmethakanozturk34@gmail.com>
2023-05-28 11:05:40 +0300

    Creating Queries

---
+++ CreatingQueries/EntityPartials/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingQueries
+{
+    public partial class Product
+    {
+        public Product() : this("Unnamed")
+        {
+
+        }
+        public Product(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ CreatingQueries/Program.cs
@@ -0,0 +1,25 @@
+﻿using CreatingQueries;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+ExampleDbContext _context = new();
+
+#region Select - Deffered Execution
+int minId = 10;
+string nameMustContains = "hakan";
+// var products = await _context.Products.ToListAsync();
+
+var products2 = from selectedProducts in _context.Products
+                where selectedProducts.Id > minId && selectedProducts.Name.Contains(nameMustContains)
+                select selectedProducts;
+
+
+minId = 20;
+nameMustContains = "yeliz";
+foreach (Product product in products2)
+{
+    Console.WriteLine($"Product Name: {product.Name}");
+}
+#endregion
+
+Console.WriteLine();
