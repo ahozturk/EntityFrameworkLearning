3ec16ffc099cbdd39fa302b14aad0623977b6a29
Hakan <ahmethakanozturk34@gmail.com>
2023-06-03 10:37:10 +0300

    Quering Functions

--- GetSingleRow/Program.cs
+++ GetSingleRow/Program.cs
@@ -9,12 +9,12 @@ ExampleDbContext _context = new();
 
 //var data = await _context.Products.SingleOrDefaultAsync(p => p.Id < 0);
 
-//var data1 = await _context.Products.FirstAsync(p => p.Name == "Test");
-//var data2 = await _context.Products.FirstAsync(p => p.Name == "Test");
-//var data3 = await _context.Products.FirstAsync(p => p.Name == "Test");
-//var data4 = await _context.Products.FirstAsync(p => p.Name == "Test");
+var data1 = await _context.Products.FirstAsync(p => p.Name == "Test");
+var data2 = await _context.Products.FirstAsync(p => p.Name == "Test");
+var data3 = await _context.Products.FirstAsync(p => p.Name == "Test");
+var data4 = await _context.Products.FirstAsync(p => p.Name == "Test");
 
-//var data5 = await _context.Products.FindAsync(10);
+var data5 = await _context.Products.FindAsync(10);
 
 var data6 = await _context.Products.OrderBy(p => p.Id).LastOrDefaultAsync(p => p.Price > 20);
 
