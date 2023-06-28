dd33015876ba5551ee17403dec532e4d6b7ecd5d
Hakan <ahmethakanozturk34@gmail.com>
2023-06-28 22:19:52 +0300

    Change Tracker Mechanism

---
+++ ChangeTracker/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GroupBy;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+YourDatabaseNameContext _context = new();
+
+var product = _context.Products.FirstOrDefault(product => product.Price > 15);
+if (product != null)
+{
+    product.Price = 14;
+    var oldPrice = _context.Entry(product).OriginalValues.GetValue<decimal?>(nameof(Product.Price));
+}
+
+//_context.ChangeTracker.DetectChanges();
+//_context.ChangeTracker.AutoDetectChangesEnabled = false;
+_context.SaveChanges();
+
+var product2 = await _context.Products.FirstOrDefaultAsync(product => product.Id == 10);
+if (product2 != null)
+    _context.Products.Remove(product2);
+_context.ChangeTracker.Entries().ToList().ForEach(entry =>
+{
+    if (entry.State == EntityState.Deleted)
+    {
+        Console.WriteLine(((Product)entry.Entity).ProductName + " Deleted");
+    }
+});
+
+if (_context.ChangeTracker.HasChanges())
+{
+
+    try
+    {
+        await _context.SaveChangesAsync(false);
+        _context.ChangeTracker.AcceptAllChanges();
+    }
+    catch
+    {
+        Console.WriteLine("Some Problem Occurred When Saving! Please Check and Save Again.");
+    }
+}
+
+
+Console.WriteLine();
--- GroupBy/YourDatabaseNameContext.cs
+++ GroupBy/YourDatabaseNameContext.cs
@@ -19,7 +19,7 @@ public partial class YourDatabaseNameContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-L858T4B;Database=YourDatabaseName;User Id=hakan;Password=sifre1234;Encrypt=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-L858T4B;Database=YourDatabaseName;User Id=hakan;Password=sonunda34;Encrypt=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
